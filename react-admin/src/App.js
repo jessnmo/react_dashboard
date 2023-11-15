3be6018fbe54b276089584e65e4b8d7d59be2c79
jessnmo <jessicamo92@gmail.com>
2023-11-16 00:00:39 +0100

    install react pro side bar to build the side bar

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,6 +1,19 @@
-import TopBar from './scenes/global/TopBar';
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import TopBar from './scenes/global/TopBar';
+import Dashboard from './scenes/dashboard';
+import SideBar from './scenes/global/SideBar';
+import { Routes, Route } from 'react-router-dom';
+/* import Team from './scenes/global/team';
+import Invoices from './scenes/global/invoice';
+import Contacts from './scenes/global/contacts';
+import Bar from './scenes/global/bar';
+import Form from './scenes/global/form';
+import Line from './scenes/global/line';
+import Pie from './scenes/global/pie';
+import FAQ from './scenes/global/faq';
+import Geography from './scenes/global/geography';
+import Calendar from './scenes/calendar'; */
 
 function App() {
 	const [theme, colorMode] = useMode();
@@ -9,8 +22,22 @@ function App() {
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
 				<div className='app'>
+					<SideBar />
 					<main className='content'>
 						<TopBar />
+						<Routes>
+							<Route path='/' element={<Dashboard />}></Route>
+							{/* <Route path='/team' element={<Team />}></Route>
+							<Route path='/contacts' element={<Contacts />}></Route>
+							<Route path='/invoices' element={<Invoices />}></Route>
+							<Route path='/form' element={<Form />}></Route>
+							<Route path='/bar' element={<Bar />}></Route>
+							<Route path='/pie' element={<Pie />}></Route>
+							<Route path='/line' element={<Line />}></Route>
+							<Route path='/faq' element={<FAQ />}></Route>
+							<Route path='/geography' element={<Geography />}></Route>
+							<Route path='/calendar' element={<Calendar />}></Route> */}
+						</Routes>
 					</main>
 				</div>
 			</ThemeProvider>
--- react-admin/src/scenes/global/SideBar.jsx
+++ react-admin/src/scenes/global/SideBar.jsx
@@ -1,7 +1,76 @@
-import React from 'react';
+import { useState } from 'react';
+import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import { Box, IconButton, Typography, useTheme } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { colorToken } from '../../theme';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PeopleOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import ContactsOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
+import PieChartOutlinedIcon from '@mui/icons-material/PieChartOutlined';
+import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
 
 const SideBar = () => {
-	return <div>SideBar</div>;
+	const theme = useTheme();
+	const colors = colorToken(theme.palette.mode);
+	const [isCollapsed, setIsCollapsed] = useState(false);
+	const [selected, setSelected] = useState('Dashboard');
+
+	return (
+		<Box
+			sx={{
+				'& .pro-sidebar-inner': {
+					background: `${colors.primary[400]} !important`,
+				},
+				'& .pro-icon-wrapper': {
+					background: 'transparent !important',
+				},
+				'& .pro-inner-item': {
+					padding: '5px 35px 5px 20px !important',
+				},
+				'& .pro-inner-item :hover': {
+					color: '#868dfd !important',
+				},
+				'& .pro-menu-item.active': {
+					color: '#6870fa  !important',
+				},
+			}}
+		>
+			{!isCollapsed && (
+				<Box mb='25px'>
+					<Box display='flex' justifyContent='center' alignItems='center'>
+						<img
+							alt='profile-pic'
+							width='100px'
+							height='100px'
+							src={`../../assets/paddle.jpeg`}
+							style={{ cursor: 'pointer', borderRadius: '50%' }}
+						/>
+					</Box>
+					<Box textAlign='center'>
+						<Typography
+							variant='h2'
+							color={colors.gray[100]}
+							fontWeight='bold'
+							sx={{ m: '10px 0 0 0' }}
+						>
+							Name
+						</Typography>
+						<Typography variant='h5' color={colors.greenAccent[500]}>
+							Admin
+						</Typography>
+					</Box>
+				</Box>
+			)}
+		</Box>
+	);
 };
 
 export default SideBar;
